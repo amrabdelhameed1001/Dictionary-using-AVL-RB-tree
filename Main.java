6f6d109e82cd0a7df1f87e03a4734c2ba82ccb3c
amrabdelhameed1001 <85768785+amrabdelhameed1001@users.noreply.github.com>
2023-03-21 19:21:47 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,80 +1,213 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
+        new Main().ui();
+
+        /*avlTree <String> tree = new avlTree<>();
+        tree.insert(41);
+        tree.printTree();
+        System.out.println();
+        tree.insert(38);
+        tree.printTree();
+        System.out.println();
+        tree.insert(31);
+        tree.printTree();
+        System.out.println();
+        tree.insert(12);
+        tree.printTree();
+        System.out.println();
+
+        tree.insert(19);
+        tree.printTree();
+        System.out.println();
+
+        tree.insert(8);
+        tree.printTree();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        tree.delete(8);
+        tree.printTree();
+        System.out.println();
+        tree.delete(12);
+        tree.printTree();
+        System.out.println();
+        tree.delete(19);
+        tree.printTree();
+        System.out.println();
+        tree.delete(31);
+        tree.printTree();
+        System.out.println();
+        tree.delete(38);
+        tree.printTree();
+        System.out.println();
+        tree.delete(41);
+        tree.printTree();
+        System.out.println();*/
+
+        /*rbTree<Integer> tree = new rbTree();
+        System.out.println("BH: " + tree.getBlackHeight());
+        System.out.println("H: " + tree.height());
+        tree.insert(41);
+        tree.printTree();
+        System.out.println("\n");
+        tree.insert(38);
+        tree.printTree();
+        System.out.println("\n");
+        tree.insert(31);
+        tree.printTree();
+        System.out.println("\n");
+        tree.insert(12);
+        tree.printTree();
+        System.out.println("\n");
+        tree.insert(19);
+        tree.printTree();
+        System.out.println("\n");
+        tree.insert(8);
+        tree.printTree();
+        System.out.println("\n");
+        System.out.println("Size: " + tree.size() + "\n");
+        System.out.println("BH: " + tree.getBlackHeight());
+        System.out.println("H: " + tree.height());
+        System.out.println("\n");
+        tree.delete(19);
+        tree.printTree();
+        System.out.println("BH: " + tree.getBlackHeight());
+        System.out.println("H: " + tree.height());
+        System.out.println("Size: " + tree.size());*/
+    }
+    public void ui() {
+        String clear = new String(new char[50]).replace("\0", "\r\n");
         Scanner input = new Scanner(System.in);
+        bstTree tree;
+        tree = buildTree();
+        if(tree instanceof avlTree<?>)
+            System.out.println("Created AVL Tree!!");
+        else
+            System.out.println("Created Red-Black Tree!!");
+        System.out.println(clear);
+        while (true) {
+            System.out.println("1. Insert");
+            System.out.println("2. Delete");
+            System.out.println("3. Search");
+            System.out.println("4. Batch Insert");
+            System.out.println("5. Batch Delete");
+            System.out.println("6. Size");
+            System.out.println("7. Height");
+            System.out.println("8. Change Tree Type");
+            int option = input.nextInt();
+            input.nextLine();
+            System.out.println(clear);
+            switch (option) {
+                case 1:
+                    System.out.println("Word: ");
+                    Object insertWord = input.nextLine();
+                    String insertStatus = tree.insert(insertWord)?"Successful": "Failed";
+                    System.out.println("Insert Status: " + insertStatus + "\n");
+                    break;
+                case 2:
+                    System.out.println("Word: ");
+                    Object deleteWord = input.nextLine();
+                    String deleteStatus = tree.delete(deleteWord)?"Successful": "Failed";
+                    System.out.println("Delete Status: " + deleteStatus + "\n");
+                    break;
+                case 3:
+                    System.out.println("Word: ");
+                    Object searchWord = input.nextLine();
+                    String searchStatus = tree.search(searchWord)?"Successful": "Failed";
+                    System.out.println("Search Status: " + searchStatus + "\n");
+                    break;
+                case 4://Batch Insert: todo
+                    System.out.println("Enter the file path: ");
+                    String filePath = input.nextLine();
+                    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                        String word;
+                        int successCount = 0, failCount = 0;
+                        while ((word = reader.readLine()) != null) {
+                            if (tree.insert(word)) {
+                                successCount++;
+                            } else {
+                                failCount++;
+                            }
+                        }
+                        System.out.println("Batch Insert Complete:");
+                        System.out.println("Success: " + successCount + " Failure: " + failCount + "\n");
+                    } catch (IOException e) {
+                        System.out.println("Error: " + e.getMessage() + "\n");
+                    }
 
-        //Enter type of tree
-        System.out.print("Enter type of tree (1 for AVL, 2 for Red-Black) ");
-        int choice = input.nextInt();
+                    break;
+                case 5://Batch Delete: todo
 
-        if(choice == 1){
-            AVLTree dic1 = new AVLTree();
-
-            while(true){
-
-
-                System.out.println("if need insert press 1");
-                System.out.println("if need delete press 2");
-                System.out.println("if need search press 3");
-                System.out.println("if need batch insert press 4");
-                System.out.println("if need batch delete press 5");
-                System.out.println("if need size press 6");
-                System.out.println("if need height press 7");
-
-                int option= input.nextInt();
-                input.nextLine();
-
-
-                switch(option){
-                    case 1:
-                        System.out.println("input word which you need to insert");
-                        String word = input.nextLine();
-                        dic1.insert(word);
-                        break;
-
-                    case 2:
-
-
-
-                }
-
+                    try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+                        String line;
+                        int deleteCount = 0;
+                        while ((line = br.readLine()) != null) {
+                            boolean deletedStatus = tree.delete(line);
+                            if (deletedStatus) {
+                                deleteCount++;
+                            }
+                        }
+                        System.out.println("Deleted " + deleteCount + " words from the tree\n");
+                    } catch (IOException e) {
+                        System.out.println("Error reading file: " + e.getMessage());
+                    }
+                    break;
+                case 6:
+                    System.out.println("Size: " + tree.size() + "\n");
+                    break;
+                case 7:
+                    System.out.println("Height: " + tree.height() + "\n");
+                    break;
+                case 8:
+                    System.out.println("Are You Sure This Will Delete Your Current Tree!!\n1. Yes\t\t2. No");
+                    int choice = input.nextInt();
+                    if(choice == 1){
+                        tree = buildTree();
+                        if(tree instanceof avlTree<?>)
+                            System.out.println("Created AVL Tree!!");
+                        else
+                            System.out.println("Created Red-Black Tree!!");
+                    }
+                    System.out.println(clear);
+                    break;
             }
         }
-
-        if(choice == 2){
-            rbTree dic2 = new rbTree();
-
-            while(true){
-
-
-                System.out.println("if need insert press 1");
-                System.out.println("if need delete press 2");
-                System.out.println("if need search press 3");
-                System.out.println("if need batch insert press 4");
-                System.out.println("if need batch delete press 5");
-                System.out.println("if need size press 6");
-                System.out.println("if need height press 7");
-
-                int option= input.nextInt();
-                input.nextLine();
-
-
-                switch(option){
-                    case 1:
-                        System.out.println("input word which you need to insert");
-                        String word = input.nextLine();
-                        dic2.insert(word);
-                        break;
-
-                    case 2:
-
-
-
-                }
-
+    }
+    bstTree buildTree(){
+        System.out.println("1. AVL Tree\n2. Red-Black Tree");
+        int type = new Scanner(System.in).nextInt();
+        System.out.println("\nSelect Tree Type:\n1. String\t\t2. Integer\t\t3. Float");
+        int keyType = new Scanner(System.in).nextInt();
+        if(type == 1){
+            switch (keyType){
+                case 1:
+                    return new avlTree<String>();
+                case 2:
+                    return new avlTree<Integer>();
+                case 3:
+                    return new avlTree<Double>();
+                default:
+                    return new avlTree<String>();
             }
+        }
+        switch (keyType){
+            case 1:
+                return new rbTree<String>();
+            case 2:
+                return new rbTree<Integer>();
+            case 3:
+                return new rbTree<Double>();
+            default:
+                return new rbTree<String>();
         }
     }
 }
