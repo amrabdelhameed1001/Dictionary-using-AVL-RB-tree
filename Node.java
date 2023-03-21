e6d4b3fccf815d77465e299ec80771d294b8bcdc
amrabdelhameed1001 <85768785+amrabdelhameed1001@users.noreply.github.com>
2023-03-21 16:45:31 +0200

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,9 +1,12 @@
- public class Node {
-    int key , height;
-    Node left , right;
+public class AVLNode<T extends Comparable<T>> {
+    T key;
+    int height;
+    AVLNode<T> left, right;
 
-    Node(int d){
-        key = d;
-        height =1;
+    public AVLNode(T key) {
+        this.key = key;
+        this.height = 1;
+        this.left = null;
+        this.right = null;
     }
 }
